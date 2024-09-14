649d2f717b9715e510262edc16a65d83d08e163d
anacabreraestefan <ana.cabreraestefan@hotmail.com>
2024-09-14 23:18:59 +0000

    tarea generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let rdm1 = Math.floor(Math.random() * who.length);
+  let rdm2 = Math.floor(Math.random() * action.length);
+  let rdm3 = Math.floor(Math.random() * what.length);
+  let rdm4 = Math.floor(Math.random() * when.length);
+  console.log(
+    who[rdm1] + " " + action[rdm2] + " " + what[rdm3] + " " + when[rdm4]
+  );
+  let texto =
+    who[rdm1] + " " + action[rdm2] + " " + what[rdm3] + " " + when[rdm4];
+  let paragraph = document.getElementById("excuse");
+  paragraph.innerText = texto;
 };
